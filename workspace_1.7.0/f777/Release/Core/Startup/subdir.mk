2476ae87acb4d43d265baa1377632005e3904eab
8evgeny <8evgeny@gmail.com>
2022-01-20 22:14:13 +0300

    +

---
+++ workspace_1.7.0/f777/Release/Core/Startup/subdir.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU ARM Embedded (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+S_SRCS += \
+../Core/Startup/startup_stm32f777iitx.s 
+
+OBJS += \
+./Core/Startup/startup_stm32f777iitx.o 
+
+S_DEPS += \
+./Core/Startup/startup_stm32f777iitx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Startup/%.o: ../Core/Startup/%.s Core/Startup/subdir.mk
+	arm-none-eabi-gcc -mcpu=cortex-m7 -c -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
+

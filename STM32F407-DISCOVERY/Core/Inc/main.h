87872f7c316f4d160e5ea9d032efd80bea3ac5fc
Evgeny Parubets <8evgeny@gmail.com>
2023-01-27 14:52:44 +0300

    add FO

--- STM32F407-DISCOVERY/Core/Inc/main.h
+++ STM32F407-DISCOVERY/Core/Inc/main.h
@@ -69,6 +69,8 @@ void Error_Handler(void);
 #define PH1_OSC_OUT_GPIO_Port GPIOH
 #define OTG_FS_PowerSwitchOn_Pin GPIO_PIN_0
 #define OTG_FS_PowerSwitchOn_GPIO_Port GPIOC
+#define SIGNAL_FO_ORANGE_Pin GPIO_PIN_1
+#define SIGNAL_FO_ORANGE_GPIO_Port GPIOC
 #define PDM_OUT_Pin GPIO_PIN_3
 #define PDM_OUT_GPIO_Port GPIOC
 #define B1_Pin GPIO_PIN_0

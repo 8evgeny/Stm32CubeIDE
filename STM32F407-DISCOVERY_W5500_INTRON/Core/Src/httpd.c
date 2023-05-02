abf83a090fa09ee01a4ef6f73931f3b3b48a8630
Evgeny Parubets <8evgeny@gmail.com>
2023-05-02 16:49:05 +0300

    Задержка перед reset

--- STM32F407-DISCOVERY_W5500_INTRON/Core/Src/httpd.c
+++ STM32F407-DISCOVERY_W5500_INTRON/Core/Src/httpd.c
@@ -587,6 +587,7 @@ void http_request(void)
         }
         if ((ipaddrNew == 1)&&(ipgateNew == 1)&&(ipmaskNew == 1)&&(destipNew == 1))
         {
+            HAL_Delay(2000);
             UART_Printf("*****  REBOOT  *****\r\n");delayUS_ASM(10000);
 
             HAL_NVIC_SystemReset();

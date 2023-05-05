04b1a627b51668a771cde1a3260ff930ec5bba83
Evgeny Parubets <8evgeny@gmail.com>
2023-05-05 17:37:56 +0300

    add Library mbedTLS

--- STM32F407-DISCOVERY_W5500_INTRON/Core/Src/main.c
+++ STM32F407-DISCOVERY_W5500_INTRON/Core/Src/main.c
@@ -24,6 +24,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "fatfs.h"
+#include "mbedtls.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -218,6 +219,7 @@ int main(void)
   MX_SPI1_Init();
   MX_SPI2_Init();
   MX_FATFS_Init();
+  MX_MBEDTLS_Init();
   /* USER CODE BEGIN 2 */
     //Сброс W5500 - уже в net_ini
 //    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_RESET);

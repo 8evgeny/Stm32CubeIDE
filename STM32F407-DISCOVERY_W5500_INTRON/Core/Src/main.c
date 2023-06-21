5d8cbb66f941644ee84d91871052b74edc65e280
Evgeny Parubets <8evgeny@gmail.com>
2023-06-21 18:58:13 +0300

    1 строка index пишется в EEPROM и оттуда работает

--- STM32F407-DISCOVERY_W5500_INTRON/Core/Src/main.c
+++ STM32F407-DISCOVERY_W5500_INTRON/Core/Src/main.c
@@ -811,10 +811,10 @@ void workI2C_EEPROM()
     if (sdCartOn == 1)
     {
         setParametersFromSD();
-//        copyParametersToAdressEEPROM(ipSettingAdressInEEPROM);
-//        copyMacToAdressEEPROM(macAdressInEEPROM);
-//        copyFileToAdressEEPROM("index.html", indexAdressInEEPROM);
-//        copyFileToAdressEEPROM("main.html", mainAdressInEEPROM);
+        copyParametersToAdressEEPROM(ipSettingAdressInEEPROM);
+        copyMacToAdressEEPROM(macAdressInEEPROM);
+        copyFileToAdressEEPROM("index.html", indexAdressInEEPROM);
+        copyFileToAdressEEPROM("main.html", mainAdressInEEPROM);
 
 //lfs не использую
 //        copyParametersToEEPROM();
@@ -1080,10 +1080,10 @@ int main(void)
     prepearUDP_PLIS();
 
     //web serverWIZ - РАБОТАЕТ
-    uint8_t i;
-    httpServer_init(TX_BUF, RX_BUF, MAX_HTTPSOCK, socknumlist);
-    wep_define_func();
-    display_reg_webContent_list();
+//    uint8_t i;
+//    httpServer_init(TX_BUF, RX_BUF, MAX_HTTPSOCK, socknumlist);
+//    wep_define_func();
+//    display_reg_webContent_list();
 
 //    tls_client_serverTest(); // работает
 //    tls_server_sizeTest(); //Web сервер WolfSSL - раскомментировать #add_definitions(-DTLS_ON)
@@ -1096,9 +1096,9 @@ int main(void)
 
 //web serverWIZ - РАБОТАЕТ
 //    for(i = 0; i < MAX_HTTPSOCK; i++) {httpServer_run(i);}
-      httpServer_run(0);
+//      httpServer_run(0);
 
-//      net_poll(); //Старый код http сервер
+      net_poll(); //Старый код http сервер
 
       sendReceiveUDP();
 

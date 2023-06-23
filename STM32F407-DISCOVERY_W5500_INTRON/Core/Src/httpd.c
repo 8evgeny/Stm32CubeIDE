6c38e4c416b764f4c31caf468e3ae3d2d9d5001d
Evgeny Parubets <8evgeny@gmail.com>
2023-06-23 11:47:03 +0300

    +

--- STM32F407-DISCOVERY_W5500_INTRON/Core/Src/httpd.c
+++ STM32F407-DISCOVERY_W5500_INTRON/Core/Src/httpd.c
@@ -679,10 +679,7 @@ void http_request(void)
         }
 
         if (strncmp (tmpbuf, "REBOOT", 6) == 0) //Перезагрузка
-        {
-            Printf("*****  REBOOT  *****\r\n");
-            HAL_NVIC_SystemReset();
-        }
+            reboot();
 
         if (strncmp (tmpbuf, "SET_PASSWORD", 12) == 0)//Смена пароля
         {

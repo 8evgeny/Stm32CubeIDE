b5b5b7fd038d4fe770dea8ea472879c41590a662
Evgeny Parubets <8evgeny@gmail.com>
2024-02-01 16:47:08 +0300

    text  в  CCM не работает

--- STM32F407-DISCOVERY_W5500_INTRON/Core/Src/main.c
+++ STM32F407-DISCOVERY_W5500_INTRON/Core/Src/main.c
@@ -40,8 +40,8 @@
 
 extern void FsForEeprom_test();
 extern void littleFsInit();
-void sendPackets(uint8_t, uint8_t* , uint16_t );
-void receivePackets(uint8_t, uint8_t* , uint16_t );
+void sendPackets(uint8_t, uint8_t* , uint16_t ) CCMRAMTEXT;
+void receivePackets(uint8_t, uint8_t* , uint16_t ) CCMRAMTEXT;
 extern int tls_client_serverTest();
 extern void tls_server_Handshake();
 extern void polarSSLTest();

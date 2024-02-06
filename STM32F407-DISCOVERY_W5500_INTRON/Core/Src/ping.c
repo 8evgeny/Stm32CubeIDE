d15fbf407ca8cfd4d42c87d1a55a7b0d8c166d08
Evgeniy Parubets <evgeniy.parubets@git-holding.ru>
2024-02-06 17:47:28 +0300

    нет  htons  checksum

--- STM32F407-DISCOVERY_W5500_INTRON/Core/Src/ping.c
+++ STM32F407-DISCOVERY_W5500_INTRON/Core/Src/ping.c
@@ -48,59 +48,8 @@ void pingCheck(){
 }
 void workInPingMode() {
     printf("Work in PING mode\r\n");
-    PINGMSGR PingRequest;
-    PingRequest.Type  = PING_REQUEST; // Ping-Request
-    PingRequest.Code = CODE_ZERO;
-//    PingRequest.ID = htons(RandomID++); // set ping-request's ID to random integer value
-//    PingRequest.SeqNum = htons(RandomSeqNum++);
-    PingRequest.CheckSum = 0;
-//    PingRequest.CheckSum = htons(checksum((uint8_t *)&PingRequest,sizeof(PingRequest)));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//    socket(PING_SOCKET, Sn_MR_IPRAW, LOCAL_PORT_UDP, 0);
-//    while(getSn_SR(PING_SOCKET)!=SOCK_IPRAW);
-//    printf("open PING socket %d\r\n", PING_SOCKET);
-//    if(sendto(PING_SOCKET,(uint8_t *)&PingRequest,sizeof(PingRequest), destip, LOCAL_PORT_UDP)==0){
-//        printf( "\r\n Fail to send ping-reply packet \r\n");}
-//    uint16_t rlen = 0;
-//    int32_t len = 0;
-//    uint16_t destPort = LOCAL_PORT_UDP;
-//    while(rlen == 0){
-//        if ( (rlen = getSn_RX_RSR(PING_SOCKET) ) > 0){
-//            /* receive data from a destination */
-//            len = recvfrom(PING_SOCKET, (uint8_t *)pingDataBuf, rlen, destip, &destPort);
-//            //Выводим на печать
-//            printf("ping: "
-//                   "%.2X %.2X %.2X %.2X %.2X %.2X %.2X %.2X "
-//                   "%.2X %.2X %.2X %.2X %.2X %.2X %.2X %.2X "
-//                   "%.2X %.2X %.2X %.2X %.2X %.2X %.2X %.2X "
-//                   "%.2X %.2X %.2X %.2X %.2X %.2X %.2X %.2X "
-//                   "%.2X %.2X %.2X %.2X %.2X %.2X %.2X %.2X "
-//                   "%.2X %.2X %.2X %.2X %.2X %.2X %.2X %.2X "
-//                   "\r\n",
-//                   pingDataBuf[0], pingDataBuf[1], pingDataBuf[2], pingDataBuf[3], pingDataBuf[4], pingDataBuf[5], pingDataBuf[6], pingDataBuf[7],
-//                   pingDataBuf[8],pingDataBuf[9], pingDataBuf[10], pingDataBuf[11], pingDataBuf[12], pingDataBuf[13], pingDataBuf[14], pingDataBuf[15],
-//                   pingDataBuf[16], pingDataBuf[17], pingDataBuf[18], pingDataBuf[19], pingDataBuf[20], pingDataBuf[21], pingDataBuf[22], pingDataBuf[23],
-//                   pingDataBuf[24], pingDataBuf[25], pingDataBuf[26], pingDataBuf[27], pingDataBuf[28], pingDataBuf[29], pingDataBuf[30], pingDataBuf[31],
-//                   pingDataBuf[32], pingDataBuf[33], pingDataBuf[34], pingDataBuf[35], pingDataBuf[36], pingDataBuf[37], pingDataBuf[38], pingDataBuf[39],
-//                   pingDataBuf[40], pingDataBuf[41], pingDataBuf[42], pingDataBuf[43], pingDataBuf[44], pingDataBuf[45], pingDataBuf[46], pingDataBuf[47]
-//                   );
-//        }
-//    }
-
+    Ethernet_ping_service_deal(PING_SOCKET);
 }
 
 // ping service processing, about 50ms performed once
@@ -108,7 +57,7 @@ void Ethernet_ping_service_deal(uint8_t sn)
 {
     static uint16_t ping_cycle = 0;
     uint16_t rlen;
-    uint8_t i, res = 0;
+    uint8_t i = 0;
         // Current channel status
     uint8_t sn_sr = 0;
 
@@ -146,7 +95,7 @@ void Ethernet_ping_service_deal(uint8_t sn)
             }
             break;
         }
-        Case ping_sta_send: // Send data
+        case PING_STA_SEND: // Send data
         {
             // After the query is 1 round, disconnect
             if (ping_device_serial > 15)
@@ -157,21 +106,17 @@ void Ethernet_ping_service_deal(uint8_t sn)
             // Judgment if the query is completed
             for (i = ping_device_serial; i < 16; i++)
             {
-                if (System_Para.ethernet_ping_ip[i][0] != 0)
+                ping_device_serial = i;
+                if (ping_req == 0)
                 {
-                    ping_device_serial = i;
-                    if (ping_req == 0)
-                    {
-                        ping_rep = 0;
-                        memcpy(ping_ip_dest, System_Para.ethernet_ping_ip[i], 4);
-                        ol_print(DEBUG_CHN, 0, "Ping:%d.%d.%d.%d\r\n", (ping_ip_dest[0]), (ping_ip_dest[1]), (ping_ip_dest[2]), (ping_ip_dest[3]));
-                    }
-                    ping_req++;
-                    Ping_Request (Sn, Ping_IP_DEST); / * Send PING Request * /
-                        ping_sta = PING_STA_WAIT;
-                    ping_cnt = 0;
-                    break;
+                    ping_rep = 0;
+                    printf( "Ping:%d.%d.%d.%d\r\n", destip[0], destip[1], destip[2], destip[3]);
                 }
+                ping_req++;
+                ping_request (sn, destip); /* Send PING Request */
+                ping_sta = PING_STA_WAIT;
+                ping_cnt = 0;
+                break;
             }
             // Query
             if(i == 16)
@@ -180,15 +125,15 @@ void Ethernet_ping_service_deal(uint8_t sn)
                 break;
             }
         }
-        Case ping_sta_wait: // Waiting results
+        case PING_STA_WAIT: // Waiting results
         {
             if ((rlen = getSn_RX_RSR(sn)) > 0)
             {
-                Rlen = ping_reply (SN, Ping_IP_DEST, RLEN); / * Get reply information * /
+                rlen = ping_reply (sn, destip, rlen); /* Get reply information */
                     ping_rep++;
                 if (ping_reply_received)
                 {
-                    ol_print(DEBUG_CHN, 0, "rep%d t=%d,len=%d\n",ping_req,ping_cnt, rlen);
+                    printf("rep%d t=%d,len=%d\r\n",ping_req, ping_cnt, rlen);
                     ping_sta = PING_STA_SEND;
                     if(ping_req == 4)
                     {
@@ -196,9 +141,9 @@ void Ethernet_ping_service_deal(uint8_t sn)
                     }
                 }
             }
-            IF ((ping_cnt> 50)) // More 4 times, end query
+            if ((ping_cnt> 50)) // More 4 times, end query
             {
-                ol_print(DEBUG_CHN, 0, "rep time out\r\n\r\n");
+                printf("rep time out\r\n\r\n");
                 ping_cnt = 0;
                 ping_sta = PING_STA_SEND;
                 if(ping_req == 4)
@@ -213,29 +158,185 @@ void Ethernet_ping_service_deal(uint8_t sn)
             // ping after 4 times, ping Next device
             if (ping_req == 5)
             {
-                ol_print(DEBUG_CHN, 0, "ping %d.%d.%d.%d, s=4, r=%d, l=%d\n",
-                         ping_ip_dest[0], ping_ip_dest[1], ping_ip_dest[2], ping_ip_dest[3], ping_rep, 4 - ping_rep);
-                if (ping_rep == 0)
-                {
-                    System_Sta.ping_list_sta &= ~(0x0001 << ping_device_serial);
-                }
-                else
-                {
-                    System_Sta.ping_list_sta |= (0x0001 << ping_device_serial);
-                }
+                printf("ping %d.%d.%d.%d, s=4, r=%d, l=%d\r\n",
+                         destip[0], destip[1], destip[2], destip[3], ping_rep, 4 - ping_rep);
                 ping_rep = 0;
                 ping_req = 0;
                 ping_device_serial++;
             }
             break;
         }
-        Case ping_sta_close: // Turn off the channel
+        case PING_STA_CLOSE: // Turn off the channel
         {
-            ol_print(DEBUG_CHN, 0, "Ping socket close\n");
-            w5500_close(sn);
+            printf("Ping socket close\r\n");
+            close(sn);
             ping_sta = PING_STA_FREE;
             break;
         }
     }
 }
 
+uint8_t ping_count(uint8_t sn, uint16_t pCount, uint8_t *addr)
+{
+    uint16_t rlen, cnt, i;
+
+    ping_reply_received = 0;
+    ping_req = 0;
+    ping_rep = 0;
+    printf( "Ping:%d.%d.%d.%d\r\n", (addr[0]), (addr[1]), (addr[2]), (addr[3]));
+
+    for (i = 0; i <pCount + 1; i ++) /* loop PCOUNT */
+    {
+        switch (getSn_SR (sn)) /* Get the Socket status */
+        {
+            case SOCK_CLOSED: /* Socket Off Status */
+            {
+                close(sn);
+                /* Create Socket */
+                WIZCHIP_WRITE (Sn_PROTO (sn), IPPROTO_ICMP); /* Set ICMP Protocol */
+                    if (socket(sn, Sn_MR_IPRAW, PING_BIND_PORT, 0) != 0) /* Determine if the IP RAW mode socket is turned on */
+                {
+                }
+                /* Check socket register */
+                while (getSn_SR(sn) != SOCK_IPRAW)
+                {
+                    HAL_Delay(50);
+                };
+                break;
+            }
+            case SOCK_IPRAW: /* IP RAW Mode */
+            {
+                cnt = 0;
+                ping_request (sn, addr); /* Send PING Request */
+                ping_req++;
+                while (1)
+                {
+                    if ((rlen = getSn_RX_RSR(sn)) > 0)
+                    {
+                        rlen = ping_reply (sn, addr, rlen); /* Get reply information */
+                        ping_rep++;
+                        if (ping_reply_received)
+                        {
+                            printf("rep t=%d,len=%d\r\n", cnt, rlen);
+                            break;
+                        }
+                    }
+                    if ((cnt > 50))
+                    {
+                        printf( "rep time out\r\n\r\n");
+                        cnt = 0;
+                        break;
+                    }
+                    else
+                    {
+                        cnt++;
+                        HAL_Delay(10);
+                    }
+                }
+                break;
+            }
+        default:
+            break;
+        }
+        if (ping_req >= pCount)
+        {
+            close(sn);
+        }
+    }
+    return ping_rep;
+}
+
+uint8_t ping_request(uint8_t sn, uint8_t *addr)
+{
+//    uint8_t *buffer;
+    uint16_t i/*, temp_len*/ = 0;
+    ping_reply_received = 0; /* ping Reply Initialization Sign */
+        PingRequest.Type = PING_REQUEST;				 /*Ping-Request*/
+    PingRequest.Code = CODE_ZERO; /* Always '0' */
+        PingRequest.ID = htons (ping_RandomID ++); /* Setting the ping response ID is a random integer variable */
+        PingRequest.SeqNum = htons (ping_RandomSeqNum ++); /* Set the serial number of the ping response is a random shaping variable */
+        for (i = 0; i < PING_BUF_LEN; i++)
+    {
+        PingRequest.Data [i] = (i)% 8; /* ping the corresponding number in '0' ~ '8' */
+    }
+    PingRequest.CheckSum = 0;
+    /* Calculated response */
+        PingRequest.CheckSum = htons(checksum((uint8_t *)&PingRequest, sizeof(PingRequest)));
+
+    /* Send ping response to destination */
+        if (sendto(sn, (uint8_t *)&PingRequest, sizeof(PingRequest), addr, PING_BIND_PORT) == 0)
+    {
+            printf( "Fail to send ping-reply packet\r\n");
+    }
+    else
+    {
+        printf("ping send\r\n");
+    }
+    return 0;
+}
+
+uint8_t ping_reply(uint8_t sn, uint8_t *addr, uint16_t rlen)
+{
+    uint16_t tmp_checksum;
+    uint16_t len;
+    uint16_t i;
+
+    uint16_t port = PING_BIND_PORT;
+    PINGMSGR PingReply;
+
+    memset(ping_rep_buf, 0, sizeof(ping_rep_buf));
+    len = recvfrom( sn, ping_rep_buf, rlen, addr, &port); /* Receive data from destination */
+
+        if (ping_rep_buf[0] == PING_REPLY)
+    {
+        PingReply.Type = ping_rep_buf[0];
+        PingReply.Code = ping_rep_buf[1];
+        PingReply.CheckSum = (ping_rep_buf[3] << 8) + ping_rep_buf[2];
+        PingReply.ID = (ping_rep_buf[5] << 8) + ping_rep_buf[4];
+        PingReply.SeqNum = (ping_rep_buf[7] << 8) + ping_rep_buf[6];
+
+        for (i = 0; i < len - 8; i++)
+        {
+            PingReply.Data[i] = ping_rep_buf[8 + i];
+        }
+        tmp_checksum = ~ checksum (ping_rep_buf, len); /* Check the number of ping replies */
+            if (tmp_checksum != 0xffff)
+        {
+            printf( "tmp_checksum = %x\r\n", tmp_checksum);
+        }
+        else
+        {
+            printf( "Reply from %3d.%3d.%3d.%3d ID=%x Byte=%d\r\n\r\n", (addr[0]), (addr[1]), (addr[2]), (addr[3]), htons(PingReply.ID), (rlen + 6));
+            ping_reply_received = 1; /* Setting the ping reply sign as 1 */
+        }
+    }
+    else if (ping_rep_buf[0] == PING_REQUEST)
+    {
+        PingReply.Code = ping_rep_buf[1];
+        PingReply.Type = ping_rep_buf[2];
+        PingReply.CheckSum = (ping_rep_buf[3] << 8) + ping_rep_buf[2];
+        PingReply.ID = (ping_rep_buf[5] << 8) + ping_rep_buf[4];
+        PingReply.SeqNum = (ping_rep_buf[7] << 8) + ping_rep_buf[6];
+        for (i = 0; i < len - 8; i++)
+        {
+            PingReply.Data[i] = ping_rep_buf[8 + i];
+        }
+        tmp_checksum = PingReply.CheckSum; /* Check ping reply number */
+            PingReply.CheckSum = 0;
+        if (tmp_checksum != PingReply.CheckSum)
+        {
+                printf(" \n CheckSum is in correct %x shold be %x \n", (tmp_checksum), htons(PingReply.CheckSum));
+        }
+        else
+        {
+        }
+        printf(" Request from %d.%d.%d.%d  ID:%x SeqNum:%x  :data size %d bytes\r\n",
+                 (addr[0]), (addr[1]), (addr[2]), (addr[3]), (PingReply.ID), (PingReply.SeqNum), (rlen + 6));
+        ping_reply_received = 1; /* Setting the ping reply sign as 1 */
+    }
+    else
+    {
+        printf("Unkonwn msg. \n");
+    }
+    return 0;
+}

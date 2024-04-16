34c6ade0acbc9fb0769d923a5c8a79ac7afaca6c
Evgeny Parubets <8evgeny@gmail.com>
2024-04-16 10:43:04 +0300

    FIX  вернул контроль и увеличил время

--- STM32F407-DISCOVERY_W5500_INTRON/Core/Src/main.c
+++ STM32F407-DISCOVERY_W5500_INTRON/Core/Src/main.c
@@ -1589,7 +1589,7 @@ void sendReceiveUDP(uint8_t udpSocket)
                 }//100
             }//совпало
 
-            if (numBadPackets2Channel == 5000) {//За 7.5 секунды связь не встала
+            if (numBadPackets2Channel == 10000) {//За 15 секунды связь не встала
 //            if (numBadPackets2Channel == 27000) {//За 40 секунд связь не встала (666*40)
 // Команда абоненту на перезагрузку
                 sendto(udpSocket, (uint8_t *)commandfromBaseToAbonentReboot, MAX_PACKET_LEN, destip, local_port_udp);

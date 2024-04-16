422f0d7c46c9289719e6cb14bb45df97c4e62726
Evgeniy Parubets <evgeniy.parubets@git-holding.ru>
2024-04-16 16:33:06 +0300

    ++

--- STM32F407-DISCOVERY_W5500_INTRON/Core/Src/main.c
+++ STM32F407-DISCOVERY_W5500_INTRON/Core/Src/main.c
@@ -1573,8 +1573,7 @@ void sendReceiveUDP(uint8_t udpSocket)
                 }//100
             }//совпало
 
-            if (numBadPackets2Channel == 10000) {//За 15 секунды связь не встала
-//            if (numBadPackets2Channel == 27000) {//За 40 секунд связь не встала (666*40)
+            if (numBadPackets2Channel == 6700) {//За 10 секунды связь не встала (666*10)
 // Команда абоненту на перезагрузку
                 sendto(udpSocket, (uint8_t *)commandfromBaseToAbonentReboot, MAX_PACKET_LEN, destip, local_port_udp);
                 printf("Sending command Reboot to abonent\r\n");
